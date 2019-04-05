94987d7c462b69d592c641a0335fd0276cb8a6f7
camebastos <camebastos@gmail.com>
2019-04-05 13:02:02 -0300

    criado botão excluir para cada postagem

---
+++ postagem.js
@@ -0,0 +1,40 @@
+const database = firebase.database()
+
+$(document).ready(function(){
+
+    database.ref('post/').once('value').then(function(snapshot) {
+        //var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        snapshot.forEach(function(childSnapshot) {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+            console.log(childData);
+            createPost(childData.text);
+            $('ul').append(`<li>${childData.text}</li>`)
+            // ...
+          });
+        console.log(snapshot.val().text);
+        //$('ul').append(`<li>${snapshot.val().texto}</li>`)
+
+        // ...
+      });
+    
+    $("#post-btn").click(function(e){ 
+        const txt = $('#post').val();
+        $('#post').val(" ");
+        createPost(txt);
+
+
+        //$('ul').append(`<li>${txt}</li>`)
+
+        database.ref('post/').push({
+            text:txt
+          });
+
+    })
+
+    function createPost(txt) {
+      $('ul').append(`<li><span>${txt}</span><button>Excluir</button></li>`)
+
+    }
+
+});
