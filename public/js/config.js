36f1e257a18d4d696b4a3d54d348eba4c25be6a2
varLeticia <leticiademichelli@gmail.com>
2019-07-13 23:28:55 -0300

    changes on timeline style with Bootstrap

---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+  let config = {
+    apiKey: "AIzaSyBq9kHaxr1VTepERlTMnPr1mTQwiTywUx0",
+    authDomain: "rede-social-laboratoria-ee605.firebaseapp.com",
+    databaseURL: "https://rede-social-laboratoria-ee605.firebaseio.com",
+    projectId: "rede-social-laboratoria-ee605",
+    storageBucket: "rede-social-laboratoria-ee605.appspot.com",
+    messagingSenderId: "249942804307"
+  };
+  firebase.initializeApp(config);
+
+
---
+++ public/js/facebook.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){ 
+
+    $("#sign-facebook-btn").click(function(e){
+        e.preventDefault();
+        let provider = new firebase.auth.FacebookAuthProvider();
+        
+        firebase.auth().signInWithPopup(provider)
+        .then(function(result) {
+            window.location = `postagem.html?id=${result.user.uid}`;
+
+
+            if (result.credential) {
+              // Para acessar a API do facebook
+              let token = result.credential.accessToken;
+              // ...
+            }
+            // Informações do usuário
+            let user = result.user;
+
+
+          }).catch(function(error) {
+              console.log(error);
+            
+            // Erros
+            let errorCode = error.code;
+            let errorMessage = error.message;
+            
+            // Erro no email
+            let email = error.email;
+            
+            // Erro no tipo de credenciamento
+            let credential = error.credential;
+            // ...
+          });
+
+    })
+
+
+});
