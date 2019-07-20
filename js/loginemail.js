f081fa736a589c3658cdcfc5f70d6122919de421
aline-ramos <alineramos.costa@gmail.com>
2019-07-20 13:46:09 -0300

    Updates

---
+++ js/loginemail.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    //Botão Cadastro
+
+    $("#sign-up-btn").click(function (e) {
+        e.preventDefault();
+        let email = $("#login-input").val();
+        let password = $("#password-input").val();
+
+        //Criar novo usuário
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(function (response) {
+                window.location = `./postagem.html?id=${response.user.uid}`;
+
+            })
+
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(error);
+            });
+
+    })
+
+
+
+    //Botão entrar
+
+    $("#sign-in-btn").click(function (e) {
+        e.preventDefault();
+        let email = $("#login-input").val();
+        let password = $("#password-input").val();
+
+        //Fazer login com usuário já cadastrado
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(function (response) {
+                window.location = `./postagem.html?id=${response.user.uid}`;
+            })
+
+            .catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // ...
+
+            });
+
+    })
+
+
+
+
+
+});
